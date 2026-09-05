7903af9d59da86c2b17cdf40a13f539a025c0c65
jswcph <84085592+jswcph@users.noreply.github.com>
2026-09-05 20:24:56 +0800

    Update 98-argon-default.sh

--- files/etc/uci-defaults/98-argon-default.sh
+++ files/etc/uci-defaults/98-argon-default.sh
@@ -3,17 +3,13 @@
 #=================================================
 # Argon 默认主题配置
 #=================================================
+if [ ! -f /etc/config/argon ]; then
+    touch /etc/config/argon
+    uci add argon global
+fi
 
-# 默认主题颜色
-uci set argon.@global[0].primary_color='#4682B4'
-
-# 登录页背景透明度
+uci set argon.@global[0].primary='#4682B4'
+uci set argon.@global[0].dark_primary='#4A6B5D'
 uci set argon.@global[0].transparency='0.3'
-
-# 登录页背景模糊
 uci set argon.@global[0].blur='0.5'
-
-# 保存配置
 uci commit argon
-
-exit 0
